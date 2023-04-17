66478fbdf11b9a71f4df242f779cbddf6e028ba0
Andrey <komarov.andrew1988@gmail.com>
2023-04-17 19:31:47 +0300

    17/04_auth

--- Django/Django_v.3.2/venv/MarAnn/MarAnn/settings.py
+++ Django/Django_v.3.2/venv/MarAnn/MarAnn/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'courses'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'profile'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- Django/Django_v.3.2/venv/MarAnn/main/models.py
+++ Django/Django_v.3.2/venv/MarAnn/main/models.py
@@ -4,16 +4,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Task(models.Model):
-    title = models.CharField('Название статьи',max_length=100, unique=True)
-    text = models.TextField('Основной текст статьи')
-    date = models.DateTimeField(default=timezone.now)
-    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+#class Task(models.Model):
+#    title = models.CharField('Название статьи',max_length=100, unique=True)
+#    text = models.TextField('Основной текст статьи')
+ #   date = models.DateTimeField(default=timezone.now)
+  #  autor = models.ForeignKey(User, on_delete=models.CASCADE)
     #views = models.IntegerField('Просмотры', default=1)
 
-    def __str__ (self):
-        return f'Задание: {self.title}'
+   # def __str__ (self):
+    #    return f'Задание: {self.title}'
 
-    class Meta:
-        verbose_name = 'Задание'
-        verbose_name_plural = 'Задания'
+    #class Meta:
+     #   verbose_name = 'Задание'
+      #  verbose_name_plural = 'Задания'
