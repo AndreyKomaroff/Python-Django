6f0337ac007142f9982947c96812b83e3deb4f6a
Andrey <komarov.andrew1988@gmail.com>
2023-05-18 21:33:06 +0300

    18/05

---
+++ Bots/komMar/main.py
@@ -0,0 +1,26 @@
+import openai
+import telebot
+import os
+
+tg_tokken = os.getenv('TG_TOKKEN')
+
+openai.api_key = os.getenv('OPENAI_TOKKEN')
+bot = telebot.TeleBot(tg_tokken)
+
+@bot.message_handler(func=lambda _: True)
+def handle_message(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.5,
+        max_tokens=1000,
+        top_p=1.0,
+        frequency_penalty=0.5,
+        presence_penalty=0.0
+    )
+    bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
+
+bot.polling()
+
+
+
