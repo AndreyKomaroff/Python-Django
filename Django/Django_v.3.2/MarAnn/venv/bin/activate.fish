6138a26b3b0ecddcc43422bfa9620e66af37b28c
Andrey <komarov.andrew1988@gmail.com>
2023-06-05 22:57:18 +0300

    05/06

--- Django/Django_v.3.2/MarAnn/venv/bin/activate.fish
+++ Django/Django_v.3.2/MarAnn/venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/andrew/Документы/Python/Django/Django_v.3.2/venv"
+set -gx VIRTUAL_ENV "/home/andrew/Документы/Python/Django/Django_v.3.2/MarAnn/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
