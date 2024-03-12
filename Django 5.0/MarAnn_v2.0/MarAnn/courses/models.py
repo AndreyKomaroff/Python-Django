54b24db1e5e60bdc2b9812885cfceb157d64f9cb
Andrey <komarov.andrew1988@gmail.com>
2024-03-12 22:10:51 +0300

    12/03_v2.0

---
+++ Django 5.0/MarAnn_v2.0/MarAnn/courses/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
+#from django.views.generic import ListView
+
+    
+class Course(models.Model):
+    owner = models.ForeignKey(User,
+                        related_name='courses_created',
+                        on_delete=models.CASCADE, verbose_name="Создатель")
+    title = models.CharField('Название курса', max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField('Описание')
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Дата")
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('modules', kwargs={'course_slug': self.slug})
+    
+class Module(models.Model):
+    course = models.ForeignKey(Course,
+                        related_name='modules',
+                        on_delete=models.CASCADE)
+    title = models.CharField('Название модуля', max_length=200)
+    image = models.ImageField(upload_to="media/courses", default='image', verbose_name="Изображение")
+    description = models.TextField('Описание', blank=True)
+    url = models.URLField('Ссылка')
+    order = OrderField(blank=True, for_fields=['course'], verbose_name="Номер п/п")
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = 'Модуль'
+        verbose_name_plural = 'Модули'
+
+    def __str__(self):
+        return f'{self.order}. {self.title}'
+    
+class Content(models.Model):
+    module = models.ForeignKey(Module,
+                            related_name='contents',
+                            on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                            on_delete=models.CASCADE,
+                            limit_choices_to={'model__in':('text', 'video', 'image', 'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+    
+    class Meta:
+        ordering = ['order']
+
---
+++ Django 5.0/MarAnn_v2.0/MarAnn/main/views.py
@@ -0,0 +1,124 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_page
+from courses.models import Module, Course
+from main.models import Blog, Product, Structure
+from django.http import HttpResponse, HttpResponseRedirect
+import os
+
+# Create your views here.
+
+menu = [{'title': 'Главная', 'url_name': 'home'},
+    {'title': 'Блог', 'url_name': 'blog'},
+    {'title': 'Уроки', 'url_name': 'classes'}]
+
+
+def home(request):
+    items = Product.objects.all()
+    structure = Structure.objects.all()
+    context = {
+        'title': 'Главная',
+        'menu': menu,
+        'items': items,
+        'structure': structure,
+    }
+
+    return render(request, 'main/home.html', context=context)
+
+#@cache_page(60)
+def blog(request):
+    posts = Blog.objects.all()
+    context = {
+        'title': 'Блог',
+        'h1': 'Блог',
+        'posts': posts,
+        'menu': menu
+    }
+
+    return render(request, 'main/blog.html', context=context)
+
+#@cache_page(60)
+@login_required
+def classes(request):
+    courses = Course.objects.all()
+    #posts = Module.objects.all()
+
+    course_slug = request.GET.get('course', None)
+    if course_slug:
+        course = Course.objects.get(slug=course_slug)
+        modules = Module.objects.filter(course=course)
+    else:
+        course = None
+        modules = None
+
+    context = {
+        'title': 'Уроки',
+        'h1': 'Видеоуроки от Супермаркетолог',
+        'menu': menu,
+        'courses': courses,
+        'selected_course': course,
+        'modules': modules    
+    }
+    
+    return render(request, 'main/classes.html', context=context)
+
+#@cache_page(60)
+def modules_view(request, course_slug):
+    course = get_object_or_404(Course, slug=course_slug)
+    modules = course.modules.all()
+
+    context = {
+        'title': course.title,
+        'menu': menu,
+        'course': course,
+        'modules': modules
+    }
+
+    return render(request, 'main/modules.html', context=context)
+
+def redirect_to_home(request):
+    if not request.user.is_superuser:
+        return redirect('home')
+    else:
+        return None
+
+def page_not_found(request, exception):
+    return render(request, 'main/404.html', status=404)
+
+#@cache_page(60)
+def show_post(request, blog_slug):
+    post = get_object_or_404(Blog, slug=blog_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+    }
+
+    return render(request, 'main/post.html', context=context)
+
+def offer_doc(request):
+
+    context = {
+        'menu': menu,
+        'title': 'Договор оферты'
+    }
+
+    return render(request, 'main/offer_doc.html', context=context)
+
+def user_consent(request):
+
+    context = {
+        'menu': menu,
+        'title': 'Политика конфиденциальности'
+    }
+
+    return render(request, 'main/user_consent.html', context=context)
+
+
+def robots(request):
+    content = "User-agent: *\nDisallow: /admin/\nDisallow: /classes/\nDisallow: /database/"
+    response = HttpResponse(content, content_type='text/plain')
+    return response
+
+handler404 = page_not_found
